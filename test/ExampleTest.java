060424fe5442f9697f2d608a96641f90b50f4253
luis-viegas <luiscarlosviegas@icloud.com>
2022-04-03 16:59:00 +0100

    Nothing

--- test/ExampleTest.java
+++ test/ExampleTest.java
@@ -1,16 +1,24 @@
 import org.junit.Test;
 
 import pt.up.fe.comp.TestUtils;
+import pt.up.fe.specs.util.SpecsIo;
 
 public class ExampleTest {
-
-    @Test
-    public void testExpression() {
-        TestUtils.parse("2+3\n10+20\n");
-        // var parserResult = TestUtils.parse("2+3\n10+20\n");
-        // parserResult.getReports().get(0).getException().get().printStackTrace();
-        // // System.out.println();
-        // var analysisResult = TestUtils.analyse(parserResult);
+    /*
+        @Test
+        public void testAccept() {
+            var parserResult = TestUtils.parse("2+3");
+            TestUtils.noErrors(parserResult.getReports());
+        }
+        @Test
+        public void testFail() {
+            var parserResult = TestUtils.parse("while(1+1)");
+            TestUtils.mustFail(parserResult.getReports());
+        }*/
+        @Test
+        public void testFile(){
+            var result = TestUtils.parse(SpecsIo.getResource("fixtures/public/myTests/import.jmm"));
+            TestUtils.noErrors(result.getReports());
+        }
     }
 
-}
